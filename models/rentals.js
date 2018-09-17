a887f90df0298dc5b6e6f96ac82c9a8df2a71144
popeye <paddypopeye@hotmail.com>
2018-09-17 17:27:47 +0200

    Second from Visual Studio

--- models/rentals.js
+++ models/rentals.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 const mongoose = require('mongoose');
-
-const Rental = mongoose.model('Rental', new mongoose.Schema({
+const moment = require('moment');
+const rentalSchema = new mongoose.Schema({
     customer:{
         type: new mongoose.Schema({
             name:{
@@ -52,7 +52,23 @@ const Rental = mongoose.model('Rental', new mongoose.Schema({
         type: Number,
         min: 0
     }
-}));
+});
+
+rentalSchema.statics.lookup = function(customerId, movieId ){
+    return this.findOne({
+        'customer._id': customerId,
+        'movie._id': body.movieId
+    })
+};
+
+rentalSchema.methods.return = function(){
+    this.dateReturned = new Date();
+    
+    const rentalDays = moment().diff(this.dateOut, 'days');
+    this.rentalFee =  rentalDays * this.movie.dailyRentalRate;
+};
+
+const Rental = mongoose.model('Rental', rentalSchema);
 
 async function validateRental(rental) {
         const schema   = {
--- routes/returns.js
+++ routes/returns.js
@@ -1,34 +1,26 @@
 const Joi = require('joi');
-const moment = require('moment');
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
 const express = require('express');
 const router = express.Router();
 const {Rental} = require('../models/rentals');
 const {Movie} = require('../models/movies');
 
-//Display all returns endpoint
-router.post('/', [auth], async (req, res) =>{
-    const { error } = validateReturn(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+//Display all returns endpoint.
+router.post('/', [auth, validate(validateReturn)], async (req, res) =>{
     
-    const rental = await Rental.findOne({
-        'customer._id': req.body.customerId,
-        'movie._id': req.body.movieId
-    });
+    const rental = await Rental.lookUp(req.body.customerId, req.body.movieId); 
     if(!rental)return res.status(404).send('Rental not found');
-    
     if(rental.dateReturned) return res.status(400).send('Return already processed');
 
-    rental.dateReturned = new Date();
-    const rentalDays = moment().diff(rental.dateOut, 'days');
-    rental.rentalFee =  rentalDays * rental.movie.dailyRentalRate;
+    rental.return();
     await rental.save();
 
     await Movie.update({_id: rental.movie._id},{
-        $inc: { numberInStock: 1 }         
+        $inc: { number InStock: 1 }         
      });
 
-    return res.status(200).send(rental);
+    return res.send(rental);
 });
 
 //Function for Return validation
@@ -41,4 +33,4 @@ function validateReturn(req){
     return Joi.validate(req,schema);
 };
 
-module.exports = router; 
+module.exports = router;
